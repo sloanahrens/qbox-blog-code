e043c89f4c8e3e6a0a1ddc64ad8c2b748da6f59c
Sloan Ahrens <sloan.ahrens@gmail.com>
2015-06-07 16:25:13 -0500

    clean up flaskapp

--- ch_6_toy_saas/flaskapp.py
+++ ch_6_toy_saas/flaskapp.py
@@ -5,7 +5,6 @@ from datetime import datetime
 
 from flask import Flask, request, render_template, session, flash, redirect, \
     url_for, jsonify
-from flask.ext.mail import Mail, Message
 
 from celery import Celery
 
@@ -15,21 +14,11 @@ from elasticsearch import Elasticsearch
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
 
-# # Flask-Mail configuration
-# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-# app.config['MAIL_PORT'] = 587
-# app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-# app.config['MAIL_DEFAULT_SENDER'] = 'flask@example.com'
 
 # Celery configuration
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-
-# Initialize extensions
-mail = Mail(app)
 
 # Initialize Celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
